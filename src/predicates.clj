ed9c562b07f008b9fccce807281d75059001b088
abir1729 <abir.cse1990@gmail.com>
2014-07-10 21:11:40 +0530

    implemented less-than and equal-to using functions as first class values.

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,13 +1,27 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
+
+(defn compare-func-maker [pred n]
+  (fn [k] (pred k n)))
 
 (defn less-than [n]
-  :-)
+  (compare-func-maker < n))
 
 (defn equal-to [n]
-  :-)
+  (compare-func-maker == n))
+
+(defn greater-than [n]
+  (fn [k]
+    (not (and
+          (less-than n k)
+          (equal-to n k)))))
+
+;(filter (less-than 3) [1 2 3 4 5])   ;=> (1 2)
+;(filter (less-than 4) [-2 12 3 4 0]) ;=> (-2 3 0)
+;(filter (equal-to 2) [2 1 3 2.0])    ;=> (2 2.0)
+;(filter (equal-to 2) [3 4 5 6])      ;=> ()
 
 (defn set->predicate [a-set]
   :-)
